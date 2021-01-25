612e8303a33e081bd598450edd5665c498db542b
github-ocean <62124680+github-ocean@users.noreply.github.com>
2021-01-25 14:26:33 +0530

    Lecture 10

--- Source/ToonTanks/Pawns/PawnTank.cpp
+++ Source/ToonTanks/Pawns/PawnTank.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "PawnTank.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
--- Source/ToonTanks/Pawns/PawnTank.h
+++ Source/ToonTanks/Pawns/PawnTank.h
@@ -25,7 +25,9 @@ private:
 	FVector MoveDircation;
 	FQuat RotationDirection;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
 	float MoveSpeed = 100.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
 	float RotationSpeed = 100.f;
 
 	void CalculateMoveInput(float Value);
