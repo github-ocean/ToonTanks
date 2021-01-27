e7e3fbb6c119ba108ce69f250350829c5d317a6d
github-ocean <62124680+github-ocean@users.noreply.github.com>
2021-01-27 12:59:32 +0530

    Lecture 11

---
+++ Source/ToonTanks/Pawns/PawnTurret.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PawnBase.h"
+#include "PawnTurret.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API APawnTurret : public APawnBase
+{
+	GENERATED_BODY()
+
+private:
+
+	void CheckFireCondition();
+
+	FTimerHandle FireRateTimerHandle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "ture"))
+	float FireRate = 2.0f;
+
+public:
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+};
