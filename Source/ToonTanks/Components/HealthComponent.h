73806dfdb680b9b2e824ab8e0c778ec1d01de497
github-ocean <62124680+github-ocean@users.noreply.github.com>
2021-02-03 11:09:34 +0530

    Lecture 19

---
+++ Source/ToonTanks/Components/HealthComponent.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TOONTANKS_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+private:
+
+	UPROPERTY(EditAnywhere)
+	float DefaultHealth = 100.f;
+	float Health;
+
+	class ATankGameModeBase* GameModeRef;
+
+public:	
+	// Sets default values for this component's properties
+	UHealthComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;	
+
+	UFUNCTION()
+	void TakeDamage(AActor* DamagedActor, flaot Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser);
+};
